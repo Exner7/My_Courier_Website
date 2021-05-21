08eeea68e058e67e600a066cecb648cef149fc68
Exner7 <alex.sviridov123@gmail.com>
2021-05-22 02:53:26 +0300

    draft contact page

--- blog.js
+++ blog.js
@@ -1,22 +1,75 @@
+
 var isExpanded = false;
 
-function expand() {
+// get the article buttons and add to each one an event-listener
 
-    
-    const articleInFocus = document.getElementById("focus");
+document.querySelectorAll(".article-btn").forEach( element => {
 
-    if (isExpanded) {
-        articleInFocus.style.animationName = "revert";
-        document.querySelector("#article-content-1").style.whiteSpace = "nowrap";
-        isExpanded = false;
-        document.querySelector("#the-button").innerHTML = "expand";
-    } else {
-        articleInFocus.style.animationName = "expand";
-        document.querySelector("#article-content-1").style.whiteSpace = "normal";
-    //    document.querySelector(".article-content").style.overflow = "clip";
-        isExpanded = true;
-        document.querySelector("#the-button").innerHTML = "collapse";
-    }
+    element.addEventListener( "click", event => {
 
-    console.log("isExpanded: " + isExpanded);
-}
+        const article = element.parentElement;
+
+        const articleNumber = article.id[article.id.length - 1];
+
+        // get the device screen width
+        const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+        console.log(typeof width + ":" + width)
+
+        switch (articleNumber) {
+            case "1":
+                translateYValue = (width < 768) ? "37.5%" : "25%";
+                translateXValue = "25%";
+                break;
+            case "2":
+                translateYValue = (width < 768) ? "12.5%" : "25%";
+                translateXValue = "-25%";
+                break;
+            case "3":
+                translateYValue = (width < 768) ? "-12.5%": "-25%";
+                translateXValue = "25%";
+                break;
+            case "4":
+                translateYValue = (width < 768) ? "-37.5%" : "-25%";
+                translateXValue = "-25%";
+                break;
+            default: console.log("Something went wrong in the switch statement");
+        }
+
+        document.documentElement.style.setProperty("--translateY-value", translateYValue);
+        document.documentElement.style.setProperty("--translateX-value", translateXValue);
+
+        const articleContent = document.querySelector(
+                "#article-content-" + articleNumber);
+        
+        if (isExpanded) {
+
+            article.style.animationName = "collapse";
+            articleContent.style.whiteSpace = "nowrap";
+            articleContent.style.overflow = "hidden";
+            element.innerHTML = "expand";
+
+            isExpanded = false;
+
+            setTimeout(() => {
+                article.className = "pocus";
+            }, 1000);
+
+        } else {
+            
+            article.className = "focus";
+
+            article.style.animationName = "expand";
+            articleContent.style.whiteSpace = "normal";
+            element.innerHTML = "collapse";
+
+            isExpanded = true;
+
+            setTimeout(() => {
+                articleContent.style.overflow = "scroll";
+            }, 1000);
+        }
+
+    } );
+
+} );
